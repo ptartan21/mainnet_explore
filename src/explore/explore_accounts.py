5b4343335311c70c2bef1d4df3e2406c35e168f7
Patrick Liu <patrick.liu01@gmail.com>
2021-11-10 00:18:11 -0500

    update

--- src/explore/explore_accounts.py
+++ src/explore/explore_accounts.py
@@ -3,54 +3,64 @@
 
 import requests
 import json
-import time
+from time import sleep
+from tqdm import tqdm
 
 APIKEY  = 'X3UT5IJW3ED6GMFV7TYKPTPFJBGAFR8NCW'
 tag     = 'latest'
 
-small_acc_1 = '../../data/3_small_addresses_from_13515990.txt'
-small_acc_2 = '../../data/3_small_addresses_from_13516239.txt'
+small_acc_1 = '../../data/250_small_addresses_from_13515990.txt'
+small_acc_2 = '../../data/250_small_addresses_from_13516239.txt'
 
-with open(small_acc_1) as f:
+in_file  = small_acc_1
+out_file = f'processed_{in_file}'
+
+with open(in_file) as f:
     lines = f.readlines()
-    for line in lines:
-      time.sleep(5)
+    res = []
+
+    for line in tqdm(lines):
+      # getting rate limited...
+      sleep(5)
       addr = line.strip('\n')
-      print(addr)
 
       # query etherscan api
       response_txl = requests.get(f'https://api.etherscan.io/api?module=account&action=txlist&address={addr}&sort=asc')
       response_bal = requests.get(f'https://api.etherscan.io/api?module=account&action=balance&address={addr}&tag={tag}&apikey={APIKEY}')
-      parsed_txl = json.loads(response_txl.content)
 
-      if response_txl.status_code != 200:
-        print("ERR")
+      if response_txl.status_code == 200 and response_bal.status_code == 200:
+        parsed_bal = json.loads(response_bal.content)
+        parsed_txl = json.loads(response_txl.content)
 
-      if response_txl.status_code == 200:
+        # account balance as of the latest block
+        balance = parsed_bal['result']
+
+        # total number of transactions as of the latest block
+        total_txn = len(parsed_txl['result'])
+
         # compute metrics: average value, average gas paid, total transactions
         total_value = 0
         total_gas = 0
         total_gasPrice = 0
         total_gasUsed = 0
 
-        total_txn = len(parsed_txl['result'])
-
-        print(f'aggregating information about {addr}')
-        print(total_txn)
         for tx in parsed_txl['result']:
-          if type(tx) is str:
-            print(parsed_txl['result'])
-            break
-          if type(tx) is dict:
-            total_value += int(tx['value'])
-            total_gas += int(tx['gas'])
-            total_gasUsed += int(tx['gasUsed'])
-            total_gasPrice += int(tx['gasPrice'])
+          total_value += int(tx['value'])
+          total_gas += int(tx['gas'])
+          total_gasUsed += int(tx['gasUsed'])
+          total_gasPrice += int(tx['gasPrice'])
         
-        print('average value', (total_value / (total_txn * (10 ** (18)))))
-        print('average gas: ', total_gas / total_txn)
-        print('average gas used: ', total_gasUsed / total_txn)
-        print('average gas price: ', total_gasPrice / total_txn)
+        avg_val = round(total_value / total_txn, 3)
+        avg_gas = round(total_gas / total_txn, 3)
+        avg_gasUsed = round(total_gasUsed / total_txn, 3)
+        avg_gasPrice = round(total_gasPrice / total_txn, 3)
+
+        # bal, num of txs, avg tx value, avg gas, avg gas used, avg gas price
+        res.append(f'{balance}, {total_txn}, {avg_val}, {avg_gas}, {avg_gasUsed}, {avg_gasPrice}')
+
+with open(out_file, mode='wt', encoding='utf-8') as o:
+  for r in res:
+    o.write(r + '\n')
 
 # # account balance in wei
 # if response_tok.status_code == 200:
