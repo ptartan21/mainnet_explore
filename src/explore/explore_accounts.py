32a68ea534b6bd383420c8a6a16b7a331ac24f38
Patrick Liu <patrick.liu01@gmail.com>
2021-11-13 19:48:12 -0500

    processed 250 blocks

--- src/explore/explore_accounts.py
+++ src/explore/explore_accounts.py
@@ -11,56 +11,60 @@ tag     = 'latest'
 
 small_acc_1 = '../../data/250_small_addresses_from_13515990.txt'
 small_acc_2 = '../../data/250_small_addresses_from_13516239.txt'
+large_acc_1 = '../../data/250_large_addresses_from_13516239.txt'
 
-in_file  = small_acc_1
-out_file = f'processed_{in_file}'
+in_file  = large_acc_1
+out_file = f'{in_file}_processed'
 
+lines = []
 with open(in_file) as f:
     lines = f.readlines()
-    res = []
 
-    for line in tqdm(lines):
-      # getting rate limited...
-      sleep(5)
-      addr = line.strip('\n')
+with open(out_file, mode='wt+', encoding='utf-8') as o:
+  res = []
 
-      # query etherscan api
-      response_txl = requests.get(f'https://api.etherscan.io/api?module=account&action=txlist&address={addr}&sort=asc')
-      response_bal = requests.get(f'https://api.etherscan.io/api?module=account&action=balance&address={addr}&tag={tag}&apikey={APIKEY}')
+  for line in tqdm(lines):
+    # getting rate limited...
+    sleep(5)
+    addr = line.strip('\n')
 
-      if response_txl.status_code == 200 and response_bal.status_code == 200:
-        parsed_bal = json.loads(response_bal.content)
-        parsed_txl = json.loads(response_txl.content)
+    # query etherscan api
+    response_txl = requests.get(f'https://api.etherscan.io/api?module=account&action=txlist&address={addr}&sort=asc')
+    response_bal = requests.get(f'https://api.etherscan.io/api?module=account&action=balance&address={addr}&tag={tag}&apikey={APIKEY}')
 
-        # account balance as of the latest block
-        balance = parsed_bal['result']
+    if response_txl.status_code == 200 and response_bal.status_code == 200:
+      parsed_bal = json.loads(response_bal.content)
+      parsed_txl = json.loads(response_txl.content)
 
-        # total number of transactions as of the latest block
-        total_txn = len(parsed_txl['result'])
+      # account balance as of the latest block
+      balance = parsed_bal['result']
 
-        # compute metrics: average value, average gas paid, total transactions
-        total_value = 0
-        total_gas = 0
-        total_gasPrice = 0
-        total_gasUsed = 0
+      # total number of transactions as of the latest block
+      total_txn = len(parsed_txl['result'])
 
-        for tx in parsed_txl['result']:
-          total_value += int(tx['value'])
-          total_gas += int(tx['gas'])
-          total_gasUsed += int(tx['gasUsed'])
-          total_gasPrice += int(tx['gasPrice'])
-        
-        avg_val = round(total_value / total_txn, 3)
-        avg_gas = round(total_gas / total_txn, 3)
-        avg_gasUsed = round(total_gasUsed / total_txn, 3)
-        avg_gasPrice = round(total_gasPrice / total_txn, 3)
+      # compute metrics: average value, average gas paid, total transactions
+      total_value = 0
+      total_gas = 0
+      total_gasPrice = 0
+      total_gasUsed = 0
 
-        # bal, num of txs, avg tx value, avg gas, avg gas used, avg gas price
-        res.append(f'{balance}, {total_txn}, {avg_val}, {avg_gas}, {avg_gasUsed}, {avg_gasPrice}')
+      if type(parsed_txl['result'][0]) == str:
+        continue
 
-with open(out_file, mode='wt', encoding='utf-8') as o:
-  for r in res:
-    o.write(r + '\n')
+      for tx in parsed_txl['result']:
+        total_value += int(tx['value'])
+        total_gas += int(tx['gas'])
+        total_gasUsed += int(tx['gasUsed'])
+        total_gasPrice += int(tx['gasPrice'])
+      
+      avg_val = round(total_value / total_txn, 3)
+      avg_gas = round(total_gas / total_txn, 3)
+      avg_gasUsed = round(total_gasUsed / total_txn, 3)
+      avg_gasPrice = round(total_gasPrice / total_txn, 3)
+
+      # bal, num of txs, avg tx value, avg gas, avg gas used, avg gas price
+      res.append(f'{balance}, {total_txn}, {avg_val}, {avg_gas}, {avg_gasUsed}, {avg_gasPrice}')
+      o.write(f'{balance}, {total_txn}, {avg_val}, {avg_gas}, {avg_gasUsed}, {avg_gasPrice}\n')
 
 # # account balance in wei
 # if response_tok.status_code == 200:
--- src/tools/get_large_addresses.js
+++ src/tools/get_large_addresses.js
@@ -8,11 +8,11 @@ const key3 = `fd9e83bd951b44ca804fa849c5860cf8`
 const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${key1}`))
 
 var args          = process.argv.slice(2);
-let latest        = 13516239
-// let latest        = 13515990
+// let latest        = 13516239
+let latest        = 13515990
 let num_blocks    = args[0]
 
-const writeStream = fs.createWriteStream(`../../data/${num_blocks}_small_addresses_from_${latest}.txt`);
+const writeStream = fs.createWriteStream(`../../data/${num_blocks}_large_addresses_from_${latest}.txt`);
 const pathName    = writeStream.path;
 
 let run = async () => {
@@ -34,9 +34,9 @@ let run = async () => {
     for(let i = 0; i < block.transactions.length; i++) {
       let tx = await web3.eth.getTransaction(block.transactions[i])
 
-      // tx value should be non-zero and less than 0.25ETH (small account)
+      // tx value should be non-zero
       let tx_val = parseInt(tx.value) * Math.pow(10,-18)
-      if (0.01 <= tx_val && tx_val <= 0.25) {
+      if (0.01 <= tx_val && tx_val <= 2) {
         if (tx.to !== null)
           addresses[tx.to]   = true
         
@@ -46,22 +46,22 @@ let run = async () => {
     }
   }
 
-  let small_addresses = []
+  let large_addresses = []
   for (address in addresses) {
     try {
       let balance = await web3.eth.getBalance(address)
 
-      // eth balance should be non-zero and less than 0.25ETH (~$1000)
+      // eth balance should be between 1 and 10
       let eth_bal = balance * Math.pow(10,-18)
-      if (0.01 <= eth_bal && eth_bal <= 0.25) {
-        small_addresses.push(address)
+      if (1 <= eth_bal && eth_bal <= 10) {
+        large_addresses.push(address)
       }
     } catch (err) {
       console.log(err)
     }
   }
   // write each value of the array on the file breaking line
-  small_addresses.forEach(value => writeStream.write(`${value}\n`));
+  large_addresses.forEach(value => writeStream.write(`${value}\n`));
 
   // the finish event is emitted when all data has been flushed from the stream
   writeStream.on('finish', () => {
